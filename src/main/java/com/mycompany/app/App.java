8368f8dce70d6ae0f93ca89f4c8cb6b956b0fda1
welllileeisme <welllileeisme@gmail.com>
2023-07-21 17:32:37 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! Welllileeisme@gmail.com2222";
+    private static final String MESSAGE = "Hello World! Welllileeisme";
 
     public App() {}
 
